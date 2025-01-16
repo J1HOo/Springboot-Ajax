2951b7440ed808083b4cbb823968282b42616da5
J1HOo <businessj1ho@gmail.com>
2025-01-16 10:07:52 +0900

    Avatar

--- AjaxSpringProject/src/main/java/com/kh/AjaxSpringProject/controller/IndexController.java
+++ AjaxSpringProject/src/main/java/com/kh/AjaxSpringProject/controller/IndexController.java
@@ -1,6 +1,6 @@
 package com.kh.AjaxSpringProject.controller;
 
-
+import com.kh.AjaxSpringProject.service.CafeService;
 import com.kh.AjaxSpringProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+// ---------------------------- INDEX -----------------------------------------------
 @Controller
 public class IndexController {
     @Autowired
@@ -22,8 +23,32 @@ public class IndexController {
     // userInfo.html을 보여줄 수 있도록 설정하는 controller
     @GetMapping("/detail/{id}")
     public String detail(@PathVariable int id, Model model) {
-        model.addAttribute("user",userService.getUserId(id));
+        model.addAttribute("user", userService.getUserId(id));
 
         return "userInfo";
+    }
+
+// ---------------------------- CAFE -----------------------------------------------
+
+    @Autowired
+    private CafeService cafeService;
+
+    @GetMapping("/cafe")
+    public String cafe() {
+        return "cafe";
+    }
+
+    @GetMapping("/cafe/detail/{id}")
+    public String cafeDetail(@PathVariable int id, Model model) {
+        model.addAttribute("cafe", cafeService.getCafeId(id));
+
+        return "cafeDetail";
+    }
+
+// ---------------------------- AVATAR -----------------------------------------------
+
+    @GetMapping("/avatar")
+    public String avatar() {
+        return "Avatar";
     }
 }
